ae1bf15d79f8cccfb68698868d35cefe319914fe
Owen hildreth <ohildreth@mines.edu>
2020-01-27 17:15:30 -0700

    Added GatheringCurrentNumberGet. Updated documentation.

--- Sources/XPSQ8Kit/Functions/Gathering.swift
+++ Sources/XPSQ8Kit/Functions/Gathering.swift
@@ -29,7 +29,8 @@ public extension XPSQ8Controller {
 // func Gathering.getConfiguration() -> String
 public extension XPSQ8Controller.GatheringController {
 	/// Gets a value in the global double array at the given index.
-	/// - Parameter index: The index of the array.
+    ///
+	/// - returns:  A String containing the current configuration.
 	func getConfiguration() throws -> String {
 		try controller.communicator.write(string: "GatheringConfigurationGet(char *)")
 		return try controller.communicator.read(as: String.self)
@@ -50,8 +51,29 @@ public extension XPSQ8Controller.GatheringController {
        - type: A string containing a valid configuration command.  See XPS documentation for examples.
      */
     func setConfiguration(type: String) throws {
-        let command = "GatheringConfigurationSet(\(type))"
-        try controller.communicator.write(string: command)
+        let message = "GatheringConfigurationSet(\(type))"
+        try controller.communicator.write(string: message)
+    }
+    
+
+    /** Returns the current maximum number of samples and current number during acquisition as set by the configuration.
+     
+    Implements  the void GatheringCurrentNumberGet(int* CurrentNumber, int* MaximumSamplesNumber)) XPS function
+    
+            do {
+                let tuple = try controller?.getCurrentNumber()
+                let currentNumber = tuple.currentNumber
+                let maximumSamples = tupple.maximumSamples
+            } catch {print(error)}
+     
+    */
+    func getCurrentNumber() throws -> (currentNumber: Int, maximumSamples: Int) {
+        let message = "GatheringCurrentNumberGet(int *,int *)"
+        
+        try controller.communicator.write(string: message)
+        let configuration = try controller.communicator.read(as: (Int.self, Int.self))
+        
+        return (currentNumber: configuration.0, maximumSamples: configuration.1)
     }
     
     
