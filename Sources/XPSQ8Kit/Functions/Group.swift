569285b21efd03eade58878b0a6c341906276321
Owen hildreth <ohildreth@mines.edu>
2020-02-14 17:04:14 -0700

    Updated variables on the group.moveRelative. Implemented group.moveAbsolute.

--- Sources/XPSQ8Kit/Functions/Group.swift
+++ Sources/XPSQ8Kit/Functions/Group.swift
@@ -30,15 +30,43 @@ public extension XPSQ8Controller {
 // MARK: - Group Functions
 public extension XPSQ8Controller.GroupController {
 	
-    /// This function moves the provided stage by the provided target displacement.
-    ///
-    /// - Parameters:
-    ///   - stageName: The name of the stage that will be moved.  This name should be set in the XPS hardward controller softare.  An example stageName would be: "MacroStages.X", where "MacroStages" is the name of the group that the stage belongs to while "X" is the name of the specific stage you want to move.
-    ///   - targetDisplacement: The distance in mm to move the specified stage.
-    func moveRelative(stageName: String, targetDisplacement: Double) throws {
+    /** This function moves the provided stage by the provided target displacement.
+     
+     do {
+         let data = try controller?.group.moveRelative(stage: "M.X", byDisplacement: 10)
+     } catch {print(error)}
+
+    - parameters:
+      - stage: The name of the stage that will be moved.  This name should be set in the XPS hardward controller softare.  An example stageName would be: "MacroStages.X", where "MacroStages" is the name of the group that the stage belongs to while "X" is the name of the specific stage you want to move.
+      - byDisplacement: The distance in mm to move the specified stage.
+     
+     */
+    
+    func moveRelative(stage stageName: String, byDisplacement targetDisplacement: Double) throws {
         let command = "GroupMoveRelative(\(stageName),\(targetDisplacement))"
         try controller.communicator.write(string: command)
         try controller.communicator.validateNoReturn()
     }
+    
+    
+    /**  Move the specified stage to the specified position in mm.
+       
+        do {
+            let data = try controller?.group.moveAbsolute(stage: "M.X", toLocation: 10)
+        } catch {print(error)}
+     
+     - parameters:
+       - stageName: The name of the stage that will be moved.  This name should be set in the XPS hardward controller softare.  An example stageName would be: "MacroStages.X", where "MacroStages" is the name of the group that the stage belongs to while "X" is the name of the specific stage you want to move.
+       - toLocation: The absolute position in mm to move the specified stage.
+    */
+    func moveAbsolute(stageName: String, toLocation: Double) throws {
+        let command = "GroupMoveAbsolute(\(stageName),\(targetDisplacement))"
+        try controller.communicator.write(string: command)
+        try controller.communicator.validateNoReturn()
+    }
+    
+    
+    
+    
 }
 
