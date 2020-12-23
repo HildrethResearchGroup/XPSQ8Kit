3b179fc2492ee6d00b06c8146507f0a1b21b8485
Owen Hildreth <ohildreth@mines.edu>
2020-12-23 13:04:43 -0700

    Added documentation for added dispatchQueue

--- Sources/XPSQ8Kit/XPSQ8Controller.swift
+++ Sources/XPSQ8Kit/XPSQ8Controller.swift
@@ -12,6 +12,10 @@ public class XPSQ8Controller {
 	var communicator: XPSQ8Communicator
     var identifier: String
     
+    
+    /// The instrument's dispatch queue that allows for running instrument code on another thread. Each instrument has a unique dispatch queue.
+    ///
+    /// It is reccomended to run all code that interfaces with the instrument on this dispatch queue to prevent blocking the main thread when reading/writing data to/from the instrument. Doing so will prevent GUI hangs and also allows for communicating with multiple instruments at the same time. If one or more instruments need to wait on another instrument before doing work, you can run the code for the instruments on a single instrument's dispatch queue.
     public var dispatchQueue: DispatchQueue
 	
 	/// Tries to create a controller for an instrument at the given address and port. A timeout value can optionally be specified.
@@ -22,7 +26,7 @@ public class XPSQ8Controller {
 	///
 	/// The following will try to create a controller for an XPSQ8 instrument at the address `192.168.0.254` and on port number `5001`.
 	/// ```
-	/// let communicator = try XPSQ8Communicator(address: "192.168.0.254", port: 5001)
+	/// let communicator = try XPSQ8Communicator(address: "192.168.0.254", port: 5001, identifier: "XPSQ8")
 	/// ```
 	/// 
 	/// - Parameters:
