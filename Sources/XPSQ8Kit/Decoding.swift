844648a18451a9c99f97a3bdcd84fff9c16e6ea4
OHildreth <50595345+OHildreth@users.noreply.github.com>
2024-05-31 10:35:48 -0600

    Fixed Compiler Error by limiting the number of variadic parameters.

--- Sources/XPSQ8Kit/Decoding.swift
+++ Sources/XPSQ8Kit/Decoding.swift
@@ -183,8 +183,10 @@ extension XPSQ8Communicator {
                 T8.init(xpsq8String:components[8]),
                 T9.init(xpsq8String:components[9]))
   }
+}
   
-  
+  /*
+
   /// Read for 11 variadic components.
   func read<T0: XPSQ8Decodable, T1: XPSQ8Decodable, T2: XPSQ8Decodable, T3: XPSQ8Decodable, T4: XPSQ8Decodable, T5: XPSQ8Decodable, T6: XPSQ8Decodable, T7: XPSQ8Decodable, T8: XPSQ8Decodable, T9: XPSQ8Decodable, T10: XPSQ8Decodable>(as: (T0.Type, T1.Type, T2.Type, T3.Type, T4.Type, T5.Type, T6.Type, T7.Type, T8.Type, T9.Type, T10.Type)) async throws -> (T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) {
     let components = try await readComponents()
@@ -309,4 +311,4 @@ extension XPSQ8Communicator {
                 T14.init(xpsq8String:components[14]),
                 T15.init(xpsq8String:components[15]))
   }
-}
+*/
